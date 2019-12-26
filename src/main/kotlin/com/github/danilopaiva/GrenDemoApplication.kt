3c1bd2a2a9b8c3af73141b8f48d7e49a663849fd
Danilo Paiva <danilopaivasilva@gmail.com>
2019-12-26 15:00:23 -0300

    initial project configuration

---
+++ src/main/kotlin/com/github/danilopaiva/GrenDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.github.danilopaiva
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrenDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrenDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/danilopaiva/GrenDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.danilopaiva
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrenDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
